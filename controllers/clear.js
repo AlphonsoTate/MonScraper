0c9efac73ea1544294a1a0fa1ff9a3960a068f81
Phenom Phons <alphonstate@gmail.com>
2018-10-13 07:39:13 -0600

    app working

---
+++ controllers/clear.js
@@ -0,0 +1,15 @@
+// imports models
+var db = require("../models");
+
+// exports clear function
+module.exports = {
+  clearDB: function(req, res) {
+    db.Headline.remove({})
+      .then(function() {
+        return db.Note.remove({});
+      })
+      .then(function() {
+        res.json({ ok: true });
+      });
+  }
+};
---
+++ controllers/fetch.js
@@ -0,0 +1,31 @@
+// imports models and scrape script
+var db = require("../models");
+var scrape = require("../scripts/scrape");
+
+
+// scrape function that returns json
+module.exports = {
+  scrapeHeadlines: function(req, res) {
+    return scrape()
+      .then(function(articles) {
+        return db.Headline.create(articles);
+      })
+      .then(function(dbHeadline) {
+        if (dbHeadline.length === 0) {
+          res.json({
+            message: "No new products today. Check back tomorrow!"
+          });
+        }
+        else {
+          res.json({
+            message: "Added " + dbHeadline.length + " new products!"
+          });
+        }
+      })
+      .catch(function(err) {
+        res.json({
+          message: "Scrape complete!!"
+        });
+      });
+  }
+};
